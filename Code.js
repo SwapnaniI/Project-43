8a6fd783a6ceb4137ddc2b97c474b6a2cb3d549e
SwapnaniI <74904367+SwapnaniI@users.noreply.github.com>
2021-05-18 10:53:05 +0530

    Add files via upload

---
+++ Code.js
@@ -0,0 +1,19 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50);
+    text("C U T N I F O N", 700,150);
+    text("A T E D A S B A", 100,250);
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70);
+    text("Hnt: Performs a particular task !!",700,170);
+    text("Hnt: Stores all information !!",100,270)
+// add code for displaying rest of the names and the hints.
+    
+
+}
